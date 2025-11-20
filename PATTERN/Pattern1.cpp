c766fd6c9ae09d4e75954b236e4b8ab226c6f11d
Kushal9753 <kushalsahu352@gmail.com>
2025-11-20 21:53:23 +0530

    pattern 1

---
+++ PATTERN/Pattern1.cpp
@@ -0,0 +1,17 @@
+// square pattern
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number ";
+    cin>>a;
+
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=a; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
