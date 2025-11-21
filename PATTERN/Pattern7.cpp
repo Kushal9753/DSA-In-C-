2cbe81374922f27d178fc0d9d993e969b2d25dc9
Kushal9753 <kushalsahu352@gmail.com>
2025-11-21 21:06:14 +0530

    Pattern 7

---
+++ PATTERN/Pattern7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"Enter a number ";
+    cin>>n;
+
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+            char name = 'a' + j - 1;
+            cout<<" "<<name;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
