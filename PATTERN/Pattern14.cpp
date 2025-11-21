6f55adba840f2cf59654f4590b7e76626cb1e6e6
Kushal9753 <kushalsahu352@gmail.com>
2025-11-21 22:20:39 +0530

    Pattern 14

---
+++ PATTERN/Pattern14.cpp
@@ -0,0 +1,24 @@
+//  *  *  *  *  * 
+//  *  *  *  *
+//  *  *  *
+//  *  *
+//  *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, j;
+    cout << "Enter a number ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = n-i+1; j >= 1; j--)
+        {
+            cout <<" * ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
