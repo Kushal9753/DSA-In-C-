e2c05b07ca872ca178ae06838cfad4caa8eff8c5
Kushal9753 <kushalsahu352@gmail.com>
2025-11-21 22:28:20 +0530

    method 2 for pattern 14

--- PATTERN/Pattern14.cpp
+++ PATTERN/Pattern14.cpp
@@ -4,6 +4,27 @@
 //  *  *
 //  *
 
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n, i, j;
+//     cout << "Enter a number ";
+//     cin >> n;
+
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = n-i+1; j >= 1; j--)
+//         {
+//             cout <<" * ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// Method 2
 #include <iostream>
 using namespace std;
 int main()
@@ -14,7 +35,7 @@ int main()
 
     for (i = 1; i <= n; i++)
     {
-        for (j = n-i+1; j >= 1; j--)
+        for (j = 1; j <= n - (i - 1); j++)
         {
             cout <<" * ";
         }
