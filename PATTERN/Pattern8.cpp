ddd03d19f4299ff564abe792193c6cf3091782a1
Kushal9753 <kushalsahu352@gmail.com>
2025-11-21 21:26:05 +0530

    2nd Method to solve pattern 8

--- PATTERN/Pattern8.cpp
+++ PATTERN/Pattern8.cpp
@@ -1,18 +1,38 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,i,j,k=1;
+//     cout<<"Enter a number ";
+//     cin>>n;
+
+//     for(i=1; i<=n; i++){
+        
+//         for(j=1; j<=n; j++){
+//             cout<<" "<<k;
+//             k++;
+//         }
+//         cout<<endl;
+        
+//     }
+//     return 0;
+// }
+
+
+
+// Method 2
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n,i,j,k=1;
+    int n,i,j;
     cout<<"Enter a number ";
     cin>>n;
 
     for(i=1; i<=n; i++){
-        
         for(j=1; j<=n; j++){
-            cout<<" "<<k;
-            k++;
+            cout<<(i-1)*5+j<<" ";
         }
         cout<<endl;
-        
     }
     return 0;
 }
