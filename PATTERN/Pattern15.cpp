79f7ee94322b32b606a963053b23ba4435e1eaac
Kushal9753 <kushalsahu352@gmail.com>
2025-11-21 22:49:18 +0530

    Pattern 15

---
+++ PATTERN/Pattern15.cpp
@@ -0,0 +1,25 @@
+// 1 2 3 4 5 
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, j;
+    cout << "Enter a number ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - (i - 1); j++)
+        {
+            cout <<j<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
