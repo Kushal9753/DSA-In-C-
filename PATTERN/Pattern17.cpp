aa7050dfd5026cf793029524d48e1c569c9fc0a0
Kushal9753 <kushalsahu352@gmail.com>
2025-11-22 22:57:37 +0530

    Pattern 17

---
+++ PATTERN/Pattern17.cpp
@@ -0,0 +1,54 @@
+//              * 
+//           *  *
+//        *  *  *
+//     *  *  *  *
+//  *  *  *  *  *
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j,n;
+//     cout<<"enter the number ";
+//     cin>>n;
+
+//     for(i=1; i<= n; i++){
+//         for(j=1; j<=n; j++){
+//             if(j <= n-i){
+//                 cout<<"   ";
+//             }else{
+//                 cout<<" * ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+// Method 2
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"enter the number ";
+    cin>>n;
+
+    for(i=1; i<= n; i++){
+        for(j=1; j<=n-i; j++){
+            
+                cout<<"   ";
+            }
+            for(j=1; j<=i; j++){
+                cout<<" * ";
+            }
+            cout<<endl;
+            
+        }
+        cout<<endl;
+        return 0;
+    }
